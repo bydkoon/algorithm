2d12fa92f72650ea7d1ab17948320a850684a2ff
bykoon-mytalk <60873670+bykoon@users.noreply.github.com>
2020-12-17 08:29:18 +0900

    fact 1, 2

---
+++ k/fact1.py
@@ -0,0 +1,29 @@
+
+def fact(n):
+    f = 1
+    for i in range(1, n+1):
+        f = f * i
+    return f
+print(fact(5))
+
+def fact2(n):
+    #문제1의 1부터 n까지 합 구하기
+    if n <= 1:
+        return 1
+    print(n)
+    return n + fact2(n-1)
+
+#[1,2,60,40,130]
+def fact3(n):
+    #문제2의 숫자 n개중에서 최대값찾기를 재귀호출로 구하기
+    max = n[0]
+    for i in range(0, n + 1):
+        if max > n:
+            return max
+
+    return fact3(n)
+
+print(fact3([1,2,60,40,130]))
+
+print(fact2(5))
+
